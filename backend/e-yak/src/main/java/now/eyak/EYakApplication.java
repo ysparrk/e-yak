5c7ac1f50b876324f76e12a46dec73ff2389c0a5
고범수 <rhqjatn2398@naver.com>
2023-07-17 17:33:13 +0900

    feat: 프로젝트 세팅

---
+++ backend/e-yak/src/main/java/now/eyak/EYakApplication.java
@@ -0,0 +1,13 @@
+package now.eyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EYakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EYakApplication.class, args);
+	}
+
+}
