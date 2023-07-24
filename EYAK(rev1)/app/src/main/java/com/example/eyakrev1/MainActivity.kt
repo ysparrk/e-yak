c2e1820a7350a5ba5841d717ee10aca7a36aaf7f
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-07-24 15:47:34 +0900

    add: EYAK(rev1)

--- EYAK(rev1)/app/src/main/java/com/example/eyakrev1/MainActivity.kt
+++ EYAK(rev1)/app/src/main/java/com/example/eyakrev1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.eyakrev1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eyakrev1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
