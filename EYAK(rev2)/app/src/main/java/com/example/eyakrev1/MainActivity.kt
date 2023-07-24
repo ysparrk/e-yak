6a916231efd9c4558ed9f140dace2f881ffe32c6
chk7082 <whgusrl7082@gmail.com>
2023-07-24 16:58:14 +0900

    design: toolbar & fragment setting

--- EYAK(rev2)/app/src/main/java/com/example/eyakrev1/MainActivity.kt
+++ EYAK(rev2)/app/src/main/java/com/example/eyakrev1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.eyakrev1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.eyakrev1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,80 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+        initPage()
+
+        binding.accountSetting.setOnClickListener {
+            accountSettingClick()
+        }
+
+        binding.alarmTab.setOnClickListener {
+            alarmTabClick()
+        }
+
+        binding.medicineTab.setOnClickListener {
+            medicineTabClick()
+        }
+
+        binding.familyTab.setOnClickListener {
+            familyTabClick()
+        }
+
+        binding.deviceTab.setOnClickListener {
+            deviceTabClick()
+        }
+
+        binding.eyakLogo.setOnClickListener {
+            alarmTabClick()
+        }
     }
+
+    private fun initPage() {
+        changeTabColor(0)
+
+    }
+    private fun accountSettingClick() {
+        Toast.makeText(this, "이게 되네", Toast.LENGTH_LONG).show()
+    }
+
+    private fun alarmTabClick() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFragment, AlarmFragment())
+            .commit()
+        changeTabColor(0)
+    }
+
+    private fun medicineTabClick() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFragment, MedicineFragment())
+            .commit()
+        changeTabColor(1)
+    }
+
+    private fun familyTabClick() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFragment, FamilyFragment())
+            .commit()
+        changeTabColor(2)
+    }
+
+    private fun deviceTabClick() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFragment, DeviceFragment())
+            .commit()
+        changeTabColor(3)
+    }
+
+    private fun changeTabColor(index: Int) {
+        val colorOn = "#ff757863"
+        val colorOff = "#ffF7FFE5"
+
+        binding.alarmTab.setColorFilter(Color.parseColor(if(index == 0) colorOn else colorOff))
+        binding.medicineTab.setColorFilter(Color.parseColor(if(index == 1) colorOn else colorOff))
+        binding.familyTab.setColorFilter(Color.parseColor(if(index == 2) colorOn else colorOff))
+        binding.deviceTab.setColorFilter(Color.parseColor(if(index == 3) colorOn else colorOff))
+    }
+
+
 }
