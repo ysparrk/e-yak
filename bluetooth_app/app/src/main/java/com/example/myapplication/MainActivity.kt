f1a10ab9caaefc70683cfa80624fe781bc962d3c
백서영 <seoyoungbaek98@naver.com>
2023-07-24 17:48:12 +0900

    feat: ESP32 통신연결, 간단한 데이터 주고받기. 권한 요청기능 시도

--- bluetooth_app/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ bluetooth_app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
@@ -18,22 +19,41 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.myapplication.databinding.ActivityMainBinding
+import java.io.DataOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
+import java.lang.Exception
 import java.util.UUID
 import kotlin.concurrent.thread
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+    // permissions list
+    private val PERMISSION = arrayOf(
+        Manifest.permission.BLUETOOTH,
+        Manifest.permission.BLUETOOTH_ADMIN,
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.BLUETOOTH_SCAN,
+        Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+        Manifest.permission.BLUETOOTH_ADVERTISE,
+    )
+    // flags & data to be monitored
+    private var permFlag: Boolean? = null
+    private val TAG = "myapp"   // log tag to be deleted afterwards.
+    private val REQUEST_ALL_PERMISSION: Int = 10;
     private val REQUEST_CODE_ENABLE_BT:Int = 1;
     private var deviceFindFlag = false
     private var devicePairedFlag = false
-    private var devicePaired = "ESP32-BT-TEST"
+    private var devicePaired = "ESP32-BT-TEST_2"//"DESKTOP-BLHP263" //"ESP32-BT-TEST_2"
 
+    // view binding
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -45,12 +65,26 @@ class MainActivity : AppCompatActivity() {
     private val bAdapter:BluetoothAdapter? by lazy {
         bManager.getAdapter()
     }
+    // bluetooth socket related
+    private var deviceMedic: BluetoothDevice? = null
+    private var socket: BluetoothSocket? = null
+    private var fallbackSocket: BluetoothSocket? = null
+    private var outStream: OutputStream? = null
+    private var inStream: InputStream? = null
+    private var buffer: ByteArray = ByteArray(1024)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         // check for permissions
+//        binding.pairedDeviceDelBtn.setOnClickListener {
+//            if (!checkPermissions(this, PERMISSION)) {
+//                ActivityCompat.requestPermissions(this, PERMISSION, REQUEST_ALL_PERMISSION)
+//            }
+//        }
+        permFlag = checkPermissions(this, PERMISSION)
+
 
         // check if init bluetooth adapter is done
         if (bAdapter == null) {
@@ -74,13 +108,65 @@ class MainActivity : AppCompatActivity() {
             bluetoothSearch()
         }
 
+        // create socket, make connection again.
+        binding.againBtn.setOnClickListener {
+            connectDevice(deviceMedic)
+        }
+
         // send / recv test
         binding.sendTest.setOnClickListener {
             binding.sendTest.text = "sent"
+            Log.d(TAG, "${socket} ${fallbackSocket}")
+            inStream = fallbackSocket?.inputStream
+            outStream = fallbackSocket?.outputStream
+            Log.d("myapp", "${inStream} ${outStream}")
+            Thread {
+                try {
+                    val sending = binding.inputText.text.toString()
+                    outStream?.write(sending.toByteArray())
+                    Log.d("myapp", "sending...")
+                } catch (e: IOException) {
+                    Log.d("myapp", "onCreate: error sending data")
+                }
+            }.start()
+            Thread {
+                while (!Thread.currentThread().isInterrupted) {
+                    try {
+                        val inBytes = inStream?.available()
+                        if (inBytes != null) {
+                            if (inBytes > 0) {
+                                val packetBytes = ByteArray(inBytes)
+                                inStream?.read(packetBytes)
+                                binding.recvTest.append(packetBytes.toString(Charsets.UTF_8))
+                            }
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }.start()
         }
     }
 
     //=== existing functions override / callbacks.
+    // onRequestPermissionsResult override.
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_ALL_PERMISSION -> {
+                for(gr in grantResults) {
+                    if (grantResults.isNotEmpty() && (gr == PackageManager.PERMISSION_GRANTED)) {
+                        Log.d(TAG, "grants: ${permissions} ${gr}")
+                        Toast.makeText(this, "${gr} 권한이 허용되었습니다!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        requestPermissions(permissions, REQUEST_ALL_PERMISSION)
+                        Toast.makeText(this, "${gr} 원활한 사용을 위해 앱 권한을 허용해 주십시오", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
     // onActivity Result override
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when(requestCode) {
@@ -146,6 +232,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     //=== functions implementation
+    // permission check - if false, request permissions.
+    private fun checkPermissions(context: Context?, perms: Array<String>): Boolean {
+        for (p in perms) {
+            if (ActivityCompat.checkSelfPermission(this, p) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, p)) {
+                    AlertDialog.Builder(this).apply {
+                        setTitle("권한 요청 이유")
+                        setMessage("약통 기기의 인식 및 앱의 통신에 필요합니다")
+                        setPositiveButton("권한 요청") { _, _ ->
+                            requestPermissionLauncher.launch(p)
+                        }
+                        setNegativeButton("거부", null)
+                    }.show()
+                } else {
+                    requestPermissionLauncher.launch(p)
+                }
+                return false
+            }
+        }
+        return true
+    }
+    // permission granting launcher
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { permFlag ->
+        if (permFlag) {
+            Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "not", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     // indicate bluetooth on/off
     private fun bluetoothOnOff() {
         // check and set bluetooth on/off image
@@ -195,6 +311,7 @@ class MainActivity : AppCompatActivity() {
                 val deviceHardwareAddress = device.address // MAC address
                 if (deviceName == devicePaired) {
                     devicePairedFlag = true
+                    deviceMedic = device
                     binding.pairedFlagImg.setColorFilter(ContextCompat.getColor(applicationContext, com.google.android.material.R.color.design_default_color_secondary))
                     binding.pairedText.text = "연결 됨"
                     binding.pairedDeviceText.text = devicePaired
@@ -214,14 +331,14 @@ class MainActivity : AppCompatActivity() {
     private fun connectDevice(targetDevice: BluetoothDevice?) {
         val thread = Thread {
             val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-            var socket: BluetoothSocket? = targetDevice?.createRfcommSocketToServiceRecord(uuid)
+            socket = targetDevice?.createRfcommSocketToServiceRecord(uuid)
             var clazz = socket?.remoteDevice?.javaClass
             var paramTypes = arrayOf<Class<*>>(Integer.TYPE)
             var m = clazz?.getMethod("createRfcommSocket", *paramTypes)
-            var fallbackSocket = m?.invoke(socket?.remoteDevice, Integer.valueOf(1)) as BluetoothSocket
+            fallbackSocket = m?.invoke(socket?.remoteDevice, Integer.valueOf(1)) as BluetoothSocket?
             Log.d("myapp", "${uuid} ${socket}")
             try {
-                fallbackSocket.connect()
+                fallbackSocket?.connect()
             } catch (e: java.lang.Exception) {
                 e.printStackTrace()
                 try {
