0dded7125ecfcec5d8f447994141251ad393c231
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-07-25 15:06:27 +0900

    fix: 도루마무..

--- EYAK(rev2)/app/src/main/java/com/example/eyakrev1/LoginActivity.kt
+++ EYAK(rev2)/app/src/main/java/com/example/eyakrev1/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.eyakrev1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.eyakrev1.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.tmpBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.googleLoginLinearLayout.setOnClickListener {
+            Toast.makeText(this, "구글 로그인 하고 싶어요", Toast.LENGTH_SHORT).show()
+        }
     }
 }
