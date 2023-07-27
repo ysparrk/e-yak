3fdb351eefa30cc9bc24091f84457179bd2d6230
ysparrk <youngseo0703@gmail.com>
2023-07-27 13:05:49 +0900

    test: gitlab webhook test용 코드 작성

--- backend/e-yak/src/test/java/now/eyak/EYakApplicationTests.java
+++ backend/e-yak/src/test/java/now/eyak/EYakApplicationTests.java
@@ -3,11 +3,17 @@ package now.eyak;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class EYakApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void webhook_test() {
+
 	}
 
 }
