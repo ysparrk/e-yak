6a4427deb7efabed713aa391e49658e7feeb0a63
chk7082 <whgusrl7082@gmail.com>
2023-07-28 10:51:30 +0900

    add: 구글 Oauth 클라이언트 ID 추가

--- EYAK(rev2)/app/src/main/java/com/example/eyakrev1/LoginActivity.kt
+++ EYAK(rev2)/app/src/main/java/com/example/eyakrev1/LoginActivity.kt
@@ -15,11 +15,11 @@ import com.google.android.gms.common.api.CommonStatusCodes
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
-    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
-    private var showOneTapUI = true
-    private val TAG = "LoginActivity" // 원하는 태그 이름으로 변경 가능
+//    private lateinit var oneTapClient: SignInClient
+//    private lateinit var signInRequest: BeginSignInRequest
+//    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+//    private var showOneTapUI = true
+//    private val TAG = "LoginActivity" // 원하는 태그 이름으로 변경 가능
 
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
@@ -27,23 +27,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        oneTapClient = Identity.getSignInClient(this)
-        signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                .setSupported(true)
-                .build())
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.your_web_client_id))
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
-                    .build())
-            // Automatically sign in when exactly one credential is retrieved.
-            .setAutoSelectEnabled(true)
-            .build()
 
 
         binding.tmpBtn.setOnClickListener {
@@ -53,77 +36,13 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.googleLoginLinearLayout.setOnClickListener {
-//            Toast.makeText(this, "구글 로그인 하고 싶어요", Toast.LENGTH_SHORT).show()
 
-            oneTapClient.beginSignIn(signInRequest)
-                .addOnSuccessListener(this) { result ->
-                    try {
-                        startIntentSenderForResult(
-                            result.pendingIntent.intentSender, REQ_ONE_TAP,
-                            null, 0, 0, 0, null)
-                    } catch (e: IntentSender.SendIntentException) {
-                        Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
-                    }
-                }
-                .addOnFailureListener(this) { e ->
-                    // No saved credentials found. Launch the One Tap sign-up flow, or
-                    // do nothing and continue presenting the signed-out UI.
-                    Log.d(TAG, e.localizedMessage)
-                    Toast.makeText(this, "여기까지 온다", Toast.LENGTH_SHORT).show()
-                }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        when (requestCode) {
-            REQ_ONE_TAP -> {
-                try {
-                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
-                    val idToken = credential.googleIdToken
-                    val username = credential.id
-                    val password = credential.password
-                    when {
-                        idToken != null -> {
-                            // Got an ID token from Google. Use it to authenticate
-                            // with your backend.
-                            Log.d(TAG, "Got ID token.")
-                            Toast.makeText(this, "Got ID token.", Toast.LENGTH_SHORT).show()
-                        }
-                        password != null -> {
-                            // Got a saved username and password. Use them to authenticate
-                            // with your backend.
-                            Log.d(TAG, "Got password.")
-                            Toast.makeText(this, "Got password.", Toast.LENGTH_SHORT).show()
-                        }
-                        else -> {
-                            // Shouldn't happen.
-                            Log.d(TAG, "No ID token or password!")
-                            Toast.makeText(this, "No ID token or password!", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                } catch (e: ApiException) {
-                    // ...
-                    Toast.makeText(this, "뭔가 안됨", Toast.LENGTH_SHORT).show()
 
-                    when (e.statusCode) {
-                        CommonStatusCodes.CANCELED -> {
-                            Log.d(TAG, "One-tap dialog was closed.")
-                            // Don't re-prompt the user.
-                            showOneTapUI = false
-                        }
-                        CommonStatusCodes.NETWORK_ERROR -> {
-                            Log.d(TAG, "One-tap encountered a network error.")
-                            // Try again or just ignore.
-                        }
-                        else -> {
-                            Log.d(TAG, "Couldn't get credential from result." +
-                                    " (${e.localizedMessage})")
-                        }
-                    }
-                }
-            }
-        }
     }
 }
