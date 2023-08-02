5a5f8427b2fc18072ae0d76d540834dbfd4c0ea1
ysparrk <youngseo0703@gmail.com>
2023-08-02 11:06:41 +0900

    refactor: Optimize imports

--- backend/e-yak/src/main/java/now/eyak/survey/dto/ContentEmotionResultDto.java
+++ backend/e-yak/src/main/java/now/eyak/survey/dto/ContentEmotionResultDto.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import now.eyak.survey.domain.ContentEmotionResult;
-import now.eyak.survey.domain.ContentTextResult;
 import now.eyak.survey.enumeration.ChoiceEmotion;
 
 @Getter
