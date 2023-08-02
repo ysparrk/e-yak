08d6dc3c2d4758dccde6522346c4b3e8850191ac
ysparrk <youngseo0703@gmail.com>
2023-08-02 11:05:53 +0900

    refactor: import 변경

--- backend/e-yak/src/main/java/now/eyak/survey/dto/ContentTextResultDto.java
+++ backend/e-yak/src/main/java/now/eyak/survey/dto/ContentTextResultDto.java
@@ -1,10 +1,8 @@
 package now.eyak.survey.dto;
 
-import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import now.eyak.survey.domain.SurveyContent;
 
 @Getter
 @Setter
