0b6f2ed0466e2007029359a7778a9fe871b16770
ysparrk <youngseo0703@gmail.com>
2023-08-03 17:24:46 +0900

    feat: emotion 설문조사 조회 기능 구현

--- backend/e-yak/src/main/java/now/eyak/survey/service/ContentEmotionResultServiceImpl.java
+++ backend/e-yak/src/main/java/now/eyak/survey/service/ContentEmotionResultServiceImpl.java
@@ -1,17 +1,22 @@
 package now.eyak.survey.service;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import now.eyak.member.domain.Member;
 import now.eyak.member.exception.NoSuchMemberException;
 import now.eyak.member.repository.MemberRepository;
-import now.eyak.survey.domain.ContentEmotionResult;
-import now.eyak.survey.domain.SurveyContent;
+import now.eyak.survey.domain.*;
 import now.eyak.survey.dto.request.ContentEmotionResultDto;
+import now.eyak.survey.dto.response.ContentEmotionResultResponseDto;
 import now.eyak.survey.repository.ContentEmotionResultRepository;
 import now.eyak.survey.repository.SurveyContentRepository;
+import now.eyak.survey.repository.SurveyRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -21,7 +26,9 @@ public class ContentEmotionResultServiceImpl implements ContentEmotionResultServ
     private final ContentEmotionResultRepository contentEmotionResultRepository;
     private final SurveyContentRepository surveyContentRepository;
     private final MemberRepository memberRepository;
+    private final SurveyRepository surveyRepository;
 
+    private final JPAQueryFactory queryFactory;
     /**
      * Emotion 설문 응답 저장
      * @param contentEmotionResultDto
@@ -71,6 +78,39 @@ public class ContentEmotionResultServiceImpl implements ContentEmotionResultServ
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new NoSuchMemberException("해당하는 회원 정보가 없습니다."));
 
         contentEmotionResultRepository.deleteById(contentEmotionResultId);
+
+    }
+
+    /**
+     * Emotion 설문 조회
+     * 요청받은 date, 요청한 memberId
+     * @param date
+     * @param memberId
+     * @return
+     */
+    @Transactional
+    @Override
+    public List<ContentEmotionResultResponseDto> getEmotionResultsByDateAndMember(LocalDate date, Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NoSuchMemberException("해당하는 회원 정보가 없습니다."));
+        Survey survey = surveyRepository.findByDate(date).orElseThrow(() -> new NoSuchElementException("해당하는 날짜의 설문기록이 없습니다."));
+
+        QContentEmotionResult qContentEmotionResult = QContentEmotionResult.contentEmotionResult;
+        QSurveyContent qSurveyContent = QSurveyContent.surveyContent;
+        QSurvey qSurvey = QSurvey.survey;
+
+        List<ContentEmotionResultResponseDto> emotionResults = queryFactory
+                .select(Projections.constructor(ContentEmotionResultResponseDto.class,
+                        qContentEmotionResult.member.id,
+                        qContentEmotionResult.choiceEmotion,
+                        qContentEmotionResult.createdAt,
+                        qContentEmotionResult.updatedAt))
+                .from(qContentEmotionResult)
+                .leftJoin(qContentEmotionResult.surveyContent, qSurveyContent)
+                .leftJoin(qSurveyContent.survey, qSurvey)
+                .where(qSurvey.date.eq(date).and(qContentEmotionResult.member.eq(member)))
+                .fetch();
+
+        return emotionResults;
 
     }
 
