638eafe5d098dff5cf83add61376d140eb762754
ysparrk <youngseo0703@gmail.com>
2023-08-03 17:25:38 +0900

    feat: text 설문조사 조회 기능 구현

--- backend/e-yak/src/main/java/now/eyak/survey/service/ContentTextResultService.java
+++ backend/e-yak/src/main/java/now/eyak/survey/service/ContentTextResultService.java
@@ -3,10 +3,17 @@ package now.eyak.survey.service;
 import now.eyak.survey.domain.ContentTextResult;
 import now.eyak.survey.dto.request.ContentTextResultDto;
 import now.eyak.survey.dto.request.ContentTextResultUpdateDto;
+import now.eyak.survey.dto.response.ContentTextResultResponseDto;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 public interface ContentTextResultService {
     ContentTextResult saveTextSurveyResult(ContentTextResultDto contentTextResultDto, Long memberId); // 저장
     ContentTextResult updateTextSurveyResult(ContentTextResultUpdateDto contentTextResultUpdateDto, Long memberId);  // 수정
     void deleteTextSurveyResult(Long contextTextResultId, Long memberId); // 삭제
+
+    List<ContentTextResultResponseDto> getTextResultsByDateAndMember(LocalDate date, Long memberId); // 조회
+
 }
--- backend/e-yak/src/main/java/now/eyak/survey/service/ContentTextResultServiceImpl.java
+++ backend/e-yak/src/main/java/now/eyak/survey/service/ContentTextResultServiceImpl.java
@@ -1,18 +1,23 @@
 package now.eyak.survey.service;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import now.eyak.member.domain.Member;
 import now.eyak.member.exception.NoSuchMemberException;
 import now.eyak.member.repository.MemberRepository;
-import now.eyak.survey.domain.ContentTextResult;
-import now.eyak.survey.domain.SurveyContent;
+import now.eyak.survey.domain.*;
 import now.eyak.survey.dto.request.ContentTextResultDto;
 import now.eyak.survey.dto.request.ContentTextResultUpdateDto;
+import now.eyak.survey.dto.response.ContentTextResultResponseDto;
 import now.eyak.survey.repository.ContentTextResultRepository;
 import now.eyak.survey.repository.SurveyContentRepository;
+import now.eyak.survey.repository.SurveyRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -22,7 +27,9 @@ public class ContentTextResultServiceImpl implements ContentTextResultService {
     private final ContentTextResultRepository contentTextResultRepository;
     private final SurveyContentRepository surveyContentRepository;
     private final MemberRepository memberRepository;
+    private final SurveyRepository surveyRepository;
 
+    private final JPAQueryFactory queryFactory;
     /**
      * Text설문 응답 저장
      * @param contentTextResultDto
@@ -73,6 +80,37 @@ public class ContentTextResultServiceImpl implements ContentTextResultService {
 
         contentTextResultRepository.deleteById(contextTextResultId);
 
+    }
+
+    /**
+     * Text 설문 조회
+     * @param date
+     * @param memberId
+     * @return
+     */
+    @Transactional
+    @Override
+    public List<ContentTextResultResponseDto> getTextResultsByDateAndMember(LocalDate date, Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NoSuchMemberException("해당하는 회원 정보가 없습니다."));
+        Survey survey = surveyRepository.findByDate(date).orElseThrow(() -> new NoSuchElementException("해당하는 날짜의 설문기록이 없습니다."));
+
+        QContentTextResult qContentTextResult = QContentTextResult.contentTextResult;
+        QSurveyContent qSurveyContent = QSurveyContent.surveyContent;
+        QSurvey qSurvey = QSurvey.survey;
+
+        List<ContentTextResultResponseDto> textResults = queryFactory
+                .select(Projections.constructor(ContentTextResultResponseDto.class,
+                        qContentTextResult.member.id,
+                        qContentTextResult.text,
+                        qContentTextResult.createdAt,
+                        qContentTextResult.updatedAt))
+                .from(qContentTextResult)
+                .leftJoin(qContentTextResult.surveyContent, qSurveyContent)
+                .leftJoin(qSurveyContent.survey, qSurvey)
+                .where(qSurvey.date.eq(date).and(qContentTextResult.member.eq(member)))
+                .fetch();
+
+        return textResults;
     }
 
 }
