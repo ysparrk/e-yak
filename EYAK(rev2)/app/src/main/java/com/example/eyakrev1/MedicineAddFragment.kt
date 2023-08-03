fd91b65c413b8c29c74e14d8c4369b7f1f8c3281
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-08-03 11:07:11 +0900

    add: 프래그먼트간 데이터 전송

--- EYAK(rev2)/app/src/main/java/com/example/eyakrev1/MedicineAddFragment.kt
+++ EYAK(rev2)/app/src/main/java/com/example/eyakrev1/MedicineAddFragment.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 
 class MedicineAddFragment : Fragment() {
 
@@ -20,6 +23,13 @@ class MedicineAddFragment : Fragment() {
         val layout = inflater.inflate(R.layout.fragment_medicine_add, container, false)
 
         layout.findViewById<Button>(R.id.resultBtn).setOnClickListener {
+            val resultValue = layout.findViewById<EditText>(R.id.medicineNameInput).text.toString() // 원하는 결과 값을 설정
+            val resultValue2 = layout.findViewById<EditText>(R.id.diseaseNameInput).text.toString()
+            val resultBundle = Bundle()
+            resultBundle.putString("bundleKey", resultValue)
+            resultBundle.putString("data2", resultValue2)
+            setFragmentResult("requestKey", resultBundle) // "requestKey"를 key로 사용하여 결과 설정
+
             mainActivity!!.gotoAddMedicineRsult()
         }
 
--- EYAK(rev2)/app/src/main/java/com/example/eyakrev1/MedicineAddResultFragment.kt
+++ EYAK(rev2)/app/src/main/java/com/example/eyakrev1/MedicineAddResultFragment.kt
@@ -6,10 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.setFragmentResultListener
 
 class MedicineAddResultFragment : Fragment() {
 
     lateinit var mainActivity: MainActivity
+
+    var result: String = ""
+    var result2: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // fragment-ktx artifact 코틀린 extension을 gradle에 추가해주면 리스너를 사용할 수 있게 됩니다.
+        setFragmentResultListener("requestKey") { requestKey, bundle ->
+            // 결과 값을 받는 곳입니다.
+            result = bundle.getString("bundleKey", "") ?: ""
+            result2 = bundle.getString("data2", "") ?: ""
+            // TextView의 텍스트를 결과 값으로 설정합니다.
+            view?.findViewById<TextView>(R.id.tmpText)?.text = result + result2
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +36,7 @@ class MedicineAddResultFragment : Fragment() {
 
         val layout = inflater.inflate(R.layout.fragment_medicine_add_result, container, false)
 
-
+        layout.findViewById<TextView>(R.id.tmpText).text = result
         // Inflate the layout for this fragment
         return layout
     }
