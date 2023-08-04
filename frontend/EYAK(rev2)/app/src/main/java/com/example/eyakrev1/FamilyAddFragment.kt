fa255089494496778700a6744c50fbccfd42d16b
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-08-04 11:05:10 +0900

    add: 가족 요청 보내기 프래그먼트

--- frontend/EYAK(rev2)/app/src/main/java/com/example/eyakrev1/FamilyAddFragment.kt
+++ frontend/EYAK(rev2)/app/src/main/java/com/example/eyakrev1/FamilyAddFragment.kt
@@ -1,20 +1,34 @@
 package com.example.eyakrev1
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class FamilyAddFragment : Fragment() {
+
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val layout: View = inflater.inflate(R.layout.fragment_family_add, container, false)
 
-        return inflater.inflate(R.layout.fragment_family_add, container, false)
+        layout.findViewById<Button>(R.id.requestBtn).setOnClickListener {
+            mainActivity!!.gotoEditFamily()
+        }
+
+        return layout
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mainActivity = context as MainActivity
+    }
 }
