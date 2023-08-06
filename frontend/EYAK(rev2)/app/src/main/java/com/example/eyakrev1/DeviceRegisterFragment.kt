ff4eb15a2205e25adbdf0699388e2d14340f56a5
rainbow00unicorn <seoyoungbaek98@naver.com>
2023-08-06 11:29:14 +0900

    add: 이미 페어링된 디비이스 찾기

--- frontend/EYAK(rev2)/app/src/main/java/com/example/eyakrev1/DeviceRegisterFragment.kt
+++ frontend/EYAK(rev2)/app/src/main/java/com/example/eyakrev1/DeviceRegisterFragment.kt
@@ -1,22 +1,17 @@
 package com.example.eyakrev1
 
-import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.provider.ContactsContract.CommonDataKinds.Im
 import android.provider.Settings
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -29,8 +24,6 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
-import com.example.eyakrev1.databinding.ActivityMainBinding
 import kotlin.concurrent.timer
 
 class DeviceRegisterFragment : Fragment() {
@@ -40,6 +33,9 @@ class DeviceRegisterFragment : Fragment() {
     private var permFlag: Boolean? = null
     private var devicePairedFlag = false
     private var deviceFindFlag = false
+
+    // (임시) 등록된 디바이스 이름
+    private var deviceNameSaved = "ESP32-BT-TEST_2"
 
     // 권한 리스트
     private val PERMISSION = arrayOf(
@@ -98,8 +94,19 @@ class DeviceRegisterFragment : Fragment() {
             startActivityForResult(intent, REQUEST_CODE_ENABLE_BT)
         }
 
-        // 이미 페어링된 디바이스 유무 확인
+        // 이미 등록/페어링된 디바이스 유무 확인
+        if (deviceNameSaved == "") {
+            layout.findViewById<LinearLayout>(R.id.btConnLayout).visibility = View.GONE
+        } else {
+            layout.findViewById<TextView>(R.id.btNotFindText).visibility = View.GONE
+            layout.findViewById<LinearLayout>(R.id.btConnLayout).visibility = View.VISIBLE
+            layout.findViewById<TextView>(R.id.btConnName).text = deviceNameSaved
+            bluetoothPaired()
+        }
+        // 등록 디바이스 삭제
+        layout.findViewById<Button>(R.id.btConnOffBtn).setOnClickListener {
 
+        }
 
         return layout
     }
@@ -172,5 +179,26 @@ class DeviceRegisterFragment : Fragment() {
                 if (resultCode != Activity.RESULT_OK) {}
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    // bluetooth 페어링 기기 찾기
+    private fun bluetoothPaired() {
+        val pairedDevices: Set<BluetoothDevice>? = btAdapter?.bondedDevices  //"${pairedDevices?.size}"
+        devicePairedFlag = false
+        pairedDevices?.forEach { device ->
+//            val deviceName = device.name
+//            val deviceHardwareAddress = device.address // MAC address
+            if (device.name == deviceNameSaved) {
+                devicePairedFlag = true
+                layout.findViewById<ImageView>(R.id.btConnImage).setColorFilter(Color.parseColor("#E3F2C1"))
+                layout.findViewById<TextView>(R.id.btConnState).text = "약통과 페어링 되었습니다."
+                return@forEach
+            }
+//            binding.deviceList.append("\n${deviceName} ${deviceHardwareAddress}")
+        }
+        if (devicePairedFlag == false) {
+            layout.findViewById<ImageView>(R.id.btConnImage).setColorFilter(Color.parseColor("#747679"))
+            layout.findViewById<TextView>(R.id.btConnState).text = "약통과 연결이 끊어졌습니다."
+        }
     }
 }
