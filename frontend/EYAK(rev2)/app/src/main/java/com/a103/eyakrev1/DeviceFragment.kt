ebdd67f44334ca1da30b22ef305e33552a13a966
백서영 <seoyoungbaek98@naver.com>
2023-08-08 12:36:53 +0900

    add: 디바이스UI 드래그앤 드롭

--- frontend/EYAK(rev2)/app/src/main/java/com/a103/eyakrev1/DeviceFragment.kt
+++ frontend/EYAK(rev2)/app/src/main/java/com/a103/eyakrev1/DeviceFragment.kt
@@ -4,8 +4,8 @@ import android.content.ClipData
 import android.content.ClipDescription
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
-import android.text.Layout
 import android.view.DragEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +15,6 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 
 class DeviceFragment : Fragment() {
 
@@ -50,15 +49,16 @@ class DeviceFragment : Fragment() {
             backDeviceEdit()
         }
 
-        // 리스너
-        layout.findViewById<ImageView>(R.id.btEditCell1).setOnDragListener(dragListener)
-        layout.findViewById<ImageView>(R.id.btEditCell2).setOnDragListener(dragListener)
-        layout.findViewById<ImageView>(R.id.btEditCell3).setOnDragListener(dragListener)
-        layout.findViewById<ImageView>(R.id.btEditCell4).setOnDragListener(dragListener)
-        layout.findViewById<ImageView>(R.id.btEditCell5).setOnDragListener(dragListener)
+        // 드랍 리스너
+        layout.findViewById<LinearLayout>(R.id.btEditCell1).setOnDragListener(dragListener)
+        layout.findViewById<LinearLayout>(R.id.btEditCell2).setOnDragListener(dragListener)
+        layout.findViewById<LinearLayout>(R.id.btEditCell3).setOnDragListener(dragListener)
+        layout.findViewById<LinearLayout>(R.id.btEditCell4).setOnDragListener(dragListener)
+        layout.findViewById<LinearLayout>(R.id.btEditCell5).setOnDragListener(dragListener)
+
         // 아이템
         layout.findViewById<ImageView>(R.id.testEditImage1).setOnLongClickListener {
-            val clipText = "This is clip data text"
+            val clipText = "This is clip data text 1"
             val item = ClipData.Item(clipText)
             val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
             val data = ClipData(clipText, mimeTypes, item)
@@ -66,7 +66,19 @@ class DeviceFragment : Fragment() {
             val dragShadowBuilder = View.DragShadowBuilder(it)
             it.startDragAndDrop(data, dragShadowBuilder, it, 0)
 
-            it.visibility = View.INVISIBLE
+            //it.visibility = View.INVISIBLE
+            true
+        }
+        layout.findViewById<ImageView>(R.id.testEditImage2).setOnLongClickListener {
+            val clipText = "This is clip data text 2"
+            val item = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+//            it.visibility = View.INVISIBLE
             true
         }
 
@@ -76,19 +88,24 @@ class DeviceFragment : Fragment() {
     // 드래그 리스너
     val dragListener = View.OnDragListener { view, event ->
         when(event.action) {
-            DragEvent.ACTION_DRAG_STARTED -> {
+            DragEvent.ACTION_DRAG_STARTED -> { // drag 시작, shadow 얻었을 때.
                 event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                true
             }
-            DragEvent.ACTION_DRAG_ENTERED -> {
+            DragEvent.ACTION_DRAG_ENTERED -> { // shadow가 listener 뷰에 진입
+                view.background.setTint(Color.parseColor("#FF9B9B"))
                 view.invalidate()
                 true
             }
-            DragEvent.ACTION_DRAG_LOCATION -> true
-            DragEvent.ACTION_DRAG_EXITED -> {
+            DragEvent.ACTION_DRAG_LOCATION -> true // shadow가 listener 뷰에 진입 후 지속.
+            DragEvent.ACTION_DRAG_EXITED -> { // shadow가 listener 뷰에서 나감
+                view.background.setTint(Color.parseColor("#AAABAE"))
                 view.invalidate()
                 true
             }
-            DragEvent.ACTION_DROP -> {
+            DragEvent.ACTION_DROP -> { // shadow가 listener 뷰에 드랍
+                view.background.setTint(Color.parseColor("#AAABAE"))
+
                 val item = event.clipData.getItemAt(0)
                 val dragData = item.text
                 Toast.makeText(requireActivity(), "$dragData", Toast.LENGTH_SHORT).show()
@@ -103,8 +120,9 @@ class DeviceFragment : Fragment() {
                 v.visibility = View.VISIBLE
                 true
             }
-            DragEvent.ACTION_DRAG_ENDED -> {
+            DragEvent.ACTION_DRAG_ENDED -> {  // 드래그앤 드롭 종료시 발생. action_drop 후 보장 X.
                 view.invalidate()
+                Toast.makeText(requireActivity(), "dragdrop end", Toast.LENGTH_SHORT).show()
                 true
             }
             else -> false
