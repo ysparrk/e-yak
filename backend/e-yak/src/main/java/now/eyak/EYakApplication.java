fb940c459b9896334f6071fc0cf07b05b5ee5b8b
ysparrk <youngseo0703@gmail.com>
2023-08-08 12:57:00 +0900

    feat: 스케줄링 적용

--- backend/e-yak/src/main/java/now/eyak/EYakApplication.java
+++ backend/e-yak/src/main/java/now/eyak/EYakApplication.java
@@ -2,7 +2,9 @@ package now.eyak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class EYakApplication {
 
