32982f09681ce6e735678cec86de7f616cbd9a87
chk7082 <whgusrl7082@gmail.com>
2023-08-09 13:26:12 +0900

    add: 알람 초기

--- frontend/EYAK(rev2)/app/src/main/java/com/a103/eyakrev1/AlarmListAdapter.kt
+++ frontend/EYAK(rev2)/app/src/main/java/com/a103/eyakrev1/AlarmListAdapter.kt
@@ -30,18 +30,17 @@ class AlarmListAdapter (val context: Context, val medicineAlarmList: ArrayList<M
         val view: View = LayoutInflater.from(context).inflate(R.layout.alarm_tab_list_view_item, null)
 
         /* 위에서 생성된 view를 alarm_tab_list_view_item.xml 파일의 각 View와 연결하는 과정이다. */
-        val medicineIconImageView = view.findViewById<ImageView>(R.id.medicine_icon)
+//        val medicineIconImageView = view.findViewById<ImageView>(R.id.medicine_icon)
         val medicineTimeTextView = view.findViewById<TextView>(R.id.medicine_time1)
         val medicineNameTextView = view.findViewById<TextView>(R.id.medicine_name)
         val medicineTimeLeftTextView = view.findViewById<TextView>(R.id.medicine_time_left)
         val medicineEatButton = view.findViewById<Button>(R.id.medicine_eat_button)
-        val medicineDelayButton = view.findViewById<Button>(R.id.medicine_delay_button)
 
         /* ArrayList<MedicineAlarm>의 변수 medicineAlarm의 이미지와 데이터를 ImageView와 TextView에 담는다. */
         val medicineAlarm = medicineAlarmList[position]
 
-        val resourceId = context.resources.getIdentifier(medicineAlarm.medicineIcon, "drawable", context.packageName)
-        medicineIconImageView.setImageResource(resourceId)
+//        val resourceId = context.resources.getIdentifier(medicineAlarm.medicineIcon, "drawable", context.packageName)
+//        medicineIconImageView.setImageResource(resourceId)
         medicineTimeTextView.text = medicineAlarm.medicineTime
         medicineNameTextView.text = medicineAlarm.medicineName
 
