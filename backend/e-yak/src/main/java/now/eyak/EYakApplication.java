bebeefaa7eb9bc0660698e87f1b98671c8beb3dd
ysparrk <youngseo0703@gmail.com>
2023-08-10 16:52:43 +0900

    fix: create, update시간 UTC 시간으로 설정되어 TimeZone 추가

--- backend/e-yak/src/main/java/now/eyak/EYakApplication.java
+++ backend/e-yak/src/main/java/now/eyak/EYakApplication.java
@@ -1,8 +1,11 @@
 package now.eyak;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
@@ -11,4 +14,10 @@ public class EYakApplication {
     public static void main(String[] args) {
         SpringApplication.run(EYakApplication.class, args);
     }
+
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
 }
