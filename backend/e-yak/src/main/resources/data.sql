b3cd5897b8addc95b2c04d1d9fccba247a088a22
고범수 <rhqjatn2398@naver.com>
2023-08-16 09:59:26 +0900

    feat: SurveyContent 생성 로직 수정
    스케줄링 -> 조회 시 생성

--- backend/e-yak/src/main/resources/data.sql
+++ backend/e-yak/src/main/resources/data.sql
@@ -18,3 +18,17 @@ insert into choice_status_entity(choice_status) values
     ('DIARRHEA'),
     ('INDIGESTION'),
     ('COUGH')^;
+
+create or replace procedure load_survey_contents()
+begin
+    declare survey_id bigint;
+    declare cur datetime;
+    set cur = now();
+    insert into survey(date, created_at, updated_at) values (CURDATE(), cur, cur);
+    set survey_id = last_insert_id();
+    insert into survey_content(survey_id, survey_content_type) values(survey_id, "CHOICE_STATUS");
+    insert into survey_content(survey_id, survey_content_type) values(survey_id, "CHOICE_EMOTION");
+    insert into survey_content(survey_id, survey_content_type) values(survey_id, "TEXT");
+end ^;
+
+call load_survey_contents ^;
